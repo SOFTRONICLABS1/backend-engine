a26fb1625aee6be37cbb8645e7ecf6bd01971b90
Jay <jayachandra@softroniclabs.com>
2025-09-09 20:15:44 +0530

    Android sign in functionality

--- android/app/src/main/java/com/musicapp/android/MainActivity.kt
+++ android/app/src/main/java/com/musicapp/android/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.donbraulio.tuneo
+package com.musicapp.android
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/musicapp/android/MainApplication.kt
+++ android/app/src/main/java/com/musicapp/android/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.donbraulio.tuneo
+package com.musicapp.android
 
 import android.app.Application
 import android.content.res.Configuration
